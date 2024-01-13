311b1a9045ffc7356245277fa0053193ecbed15e
uni2237 <yunhui.cho98@gmail.com>
2024-01-13 20:05:33 +0900

    refactor: none, type 테스트 추가 (수정중)

--- service/src/test.py
+++ service/src/test.py
@@ -1,25 +1,39 @@
-import unittest
-from unittest import TestCase , main
-from crawler import NaverStockNewsCrawler  
+from datetime import datetime
+from unittest import TestCase, main
+
+from crawler import NaverStockNewsCrawler, Topic, Config
+
 
 class TestNaverStockNewsCrawler(TestCase):
     def setUp(self):
-        self.crawler = NaverStockNewsCrawler("https://finance.naver.com/news/mainnews.nhn")
+        self.crawler = NaverStockNewsCrawler(Topic.STOCK, Config.STOCK_URL)
 
     def test_crawl(self):
-        result = self.crawler.crawl()
-        self.assertIsNotNone(result)  
-        self.assertIsInstance(result, list)  
-    
+        self.crawler.crawl()
+
+        # TODO: Add test case here
+
     def test_get_news_content(self):
-        link = 'https://n.news.naver.com/mnews/article/001/0014424507'
-        content,journalist = self.crawler.get_news_content_and_journalist(link)
+        link = "https://n.news.naver.com/mnews/article/001/0014424507"
+        (
+            content,
+            journalist,
+            date_time,
+            status,
+        ) = self.crawler.get_news_content_and_journalist(link)
+
+        # 결과가 None이 아닌지 확인
         self.assertIsNotNone(content)
-        self.assertIsInstance(content, str)
-
         self.assertIsNotNone(journalist)
-        self.assertIsInstance(journalist, str)
-        
+        self.assertIsNotNone(date_time)
+        self.assertIsNotNone(status)
 
-if __name__ == '__main__':
+        # 결과의 타입 확인
+        self.assertIsInstance(content, str)
+        self.assertIsInstance(journalist, str)
+        self.assertIsInstance(date_time, datetime)  # Fix: Check the type of date_time
+        self.assertIsInstance(status, bool)
+
+
+if __name__ == "__main__":
     main()
