097d817786fbb661bb9f6cb65b746a0eef33be37
uni2237 <yunhui.cho98@gmail.com>
2024-02-03 18:42:23 +0900

    feat : supabase db load 추가 수정 중 (nodename nor servname provided, or not known 에러 발생)

--- service/src/supabase_handler.py
+++ service/src/supabase_handler.py
@@ -5,6 +5,8 @@ import yaml
 
 from supabase import create_client, Client
 
+from config import Config, Topic, NewsDocuments
+
 
 class SupabaseConfig:
     def __init__(self, config_file_path):
@@ -15,11 +17,14 @@ class SupabaseConfig:
         self.load_config()
 
     def load_config(self):
-        with open(self.config_file_path) as f:
-            conf = yaml.load(f, Loader=yaml.FullLoader)
+        try:
+            with open(self.config_file_path) as f:
+                conf = yaml.load(f, Loader=yaml.FullLoader)
             self.supabase_url = conf["supabase"]["supabase_url"]
             self.supabase_key = conf["supabase"]["supabase_key"]
             self.supabase_table = conf["supabase"]["supabase_table"]
+        except Exception as e:
+            logging.error("yaml 파일 load 실패: %s", e)
 
 
 class SupabaseHandler:
@@ -58,3 +63,57 @@ class SupabaseHandler:
 
         except Exception as e:
             logging.error("An error occurred while saving to Supabase: %s", e)
+
+    def get_data_from_supabase(self):
+        try:
+            existing_record = (
+                self.client.table(self.supabase_table).select("*").execute()
+            )
+            if existing_record.data:
+                return existing_record.data
+            else:
+                logging.info("No data found in Supabase.")
+                return []
+        except Exception as e:
+            logging.error("Supabase data load 실패: %s", e)
+            return False
+
+    # 데이터를 Python dictionary 형태로 반환하는 함수 추가
+    def data_to_dict(self, data):
+        data_dict_list = []
+        for item in data:
+            data_dict = {
+                "url": item.get("url", ""),
+                "topic": item.get("topic", ""),
+                "title": item.get("title", ""),
+                "status": item.get("status", ""),
+                "content": item.get("content", ""),
+                "summary": item.get("summary", ""),
+                "press": item.get("press", ""),
+                "journalist": item.get("journalist", ""),
+                "date": item.get("date", ""),
+            }
+            data_dict_list.append(data_dict)
+        return data_dict_list
+
+
+def main():
+
+    # SupabaseConfig 객체 생성
+    supabase_config = SupabaseConfig(Config.YAML_PATH)
+    # SupabaseHandler 객체 생성
+    supabase_handler = SupabaseHandler(supabase_config)
+
+    # 데이터 가져오기
+    data = supabase_handler.get_data_from_supabase()
+
+    # 데이터를 Python dictionary 형태로 변환
+    data_dict_list = supabase_handler.data_to_dict(data)
+
+    # 반환된 데이터 사용
+    for item in data_dict_list:
+        logging.info(item)
+
+
+if __name__ == "__main__":
+    main()
