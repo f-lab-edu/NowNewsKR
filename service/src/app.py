03a3b01f8b15d99fcacff17eb9a88bfd083734a3
uni2237 <yunhui.cho98@gmail.com>
2024-02-24 14:01:45 +0900

    feat: 크롤러, 인덱서 백그라운드 task 추가

--- service/src/app.py
+++ service/src/app.py
@@ -8,6 +8,8 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from config import Config
 from supabase_handler import SupabaseHandler
 from es_handler import ElasticSearchHandler
+from crawler import NaverNewsCrawler, Topic
+from es_indexer import ESIndexer
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_dir = os.path.join(current_dir, "..", "..")
@@ -19,6 +21,16 @@ sys.path.append(os.path.join(project_dir, llm_src_path))
 
 from embedding_model import EmbeddingModel
 from llm_module import LLMModule
+
+# 싱글톤 객체 관리자를 위한 코드
+_singleton_instances = {}
+
+
+def get_singleton_instance(cls, *args, **kwargs):
+    if cls not in _singleton_instances:
+        _singleton_instances[cls] = cls(*args, **kwargs)
+    return _singleton_instances[cls]
+
 
 app = Flask(__name__)
 
@@ -50,10 +62,12 @@ class QueryApp:
         }
 
 
-embedding_model = EmbeddingModel()
-llm_model = LLMModule()
-supabase_handler = SupabaseHandler()
-es_handler = ElasticSearchHandler()
+embedding_model = get_singleton_instance(EmbeddingModel)
+llm_model = get_singleton_instance(LLMModule)
+supabase_handler = get_singleton_instance(SupabaseHandler)
+es_handler = get_singleton_instance(ElasticSearchHandler)
+es_indexer = ESIndexer(embedding_model)
+
 query_app = QueryApp(embedding_model, llm_model, supabase_handler, es_handler)
 
 
@@ -69,7 +83,42 @@ def query_endpoint():
     return json.dumps(response, ensure_ascii=False)
 
 
+def crawl_news():
+    # 모든 주제에 대해 크롤링을 수행합니다.
+    for topic in Topic:
+        crawler = NaverNewsCrawler(
+            topic=topic,
+            url=Config.STOCK_URL,
+            supabase_handler=supabase_handler,
+        )
+        crawler.crawl()
+
+
+def index_news():
+    news_db = supabase_handler.get_news_data_from_supabase()
+    news_documents = supabase_handler.data_to_news_documents(news_db)
+
+    for news_doc in news_documents:
+        index_result = es_indexer.index_data_to_elasticsearch(news_doc)
+        if index_result:
+            news_doc.is_indexed = True
+            supabase_handler.update_news_db_index_status(news_doc)
+
+
+def schedule_tasks():
+    # 크롤랑,인덱싱 백그라운드로 주기적으로 실행합니다.
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(
+        crawl_news, "interval", hours=1
+    )  # Customize the interval as needed
+    scheduler.add_job(
+        index_news, "interval", hours=1
+    )  # Customize the interval as needed
+    scheduler.start()
+
+
 if __name__ == "__main__":
+    schedule_tasks()  # 크롤러 스케줄링 시작
     app.run(debug=True, port=8080)
 
 
